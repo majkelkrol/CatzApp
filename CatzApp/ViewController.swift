f4756104363a97450ea899b96d30c35241726008
Majkel <majkelkrol@icloud.com>
2023-06-21 11:20:25 +0200

    Add tableView with simple datas

--- CatzApp/ViewController.swift
+++ CatzApp/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var tableView: UITableView!
+    
+    var breads: [String] = ["Abyssinian", "Aegean", "American Bobtail"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setup()
     }
+    
+    func setup() {
+        title = "CatzApp"
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        breads.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = breads[indexPath.row]
+        return cell
+    }
+}
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("tap")
+    }
 }
 
