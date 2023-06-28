0d9935873ab2675b971f797b0230ad8c7fbb4246
Majkel <majkelkrol@icloud.com>
2023-06-28 10:55:05 +0200

    Add BreadImage and logic about decoded image

---
+++ CatzApp/BreadImage.swift
@@ -0,0 +1,14 @@
+//
+//  BreadImage.swift
+//  CatzApp
+//
+//  Created by Majkel on 28/06/2023.
+//
+
+import Foundation
+
+struct BreadImage: Codable {
+    let id: String
+    let url: String
+    let width, height: Int
+}
--- CatzApp/CatDetailViewController.swift
+++ CatzApp/CatDetailViewController.swift
@@ -21,6 +21,11 @@ class CatDetailViewController: UIViewController {
     var carOrigin: String?
     var catDescription: String?
     
+    var url = [BreadImage]()
+    var urlId: String?
+    var urlString: String?
+    var urlImage: String = ""
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,5 +46,63 @@ class CatDetailViewController: UIViewController {
 
         descriptionLabel.text = catDescription
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        setURL(id: catID ?? "nil")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        imageView.load(urlImage)
+    }
+    
+    func setURL(id: String) {
+        urlId = "https://api.thecatapi.com/v1/images/search?breed_ids=\(id)"
+        imgJson()
+    }
+    
+    func imgJson() {
+        guard let url = URL(string: urlId ?? "nil") else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                do {
+                    if let decodedJson = try? JSONDecoder().decode([BreadImage].self, from: data) {
+                        DispatchQueue.main.async {
+                            if let decodedImage = decodedJson.last?.url  {
+                                self.urlImage = decodedImage
+                            }
+                        }
+                    }
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+        task.resume()
+        
+    }
+}
 
+extension UIImageView {
+    func load(_ urlString: String) {
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
 }
